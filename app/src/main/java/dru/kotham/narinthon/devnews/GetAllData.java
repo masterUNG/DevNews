6644e1a8722d7854497ae0324d4c6ea94717d286
kat <neko.hellomikky2012@gmail.com>
2017-05-14 11:30:31 +0700

    Authen OK

--- app/src/main/java/dru/kotham/narinthon/devnews/GetAllData.java
+++ app/src/main/java/dru/kotham/narinthon/devnews/GetAllData.java
@@ -2,6 +2,11 @@ package dru.kotham.narinthon.devnews;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 /**
  * Created by Administrator on 13/5/2560.
@@ -17,6 +22,25 @@ public class GetAllData extends AsyncTask<String, Void, String>{
 
     @Override
     protected String doInBackground(String... params) {
-        return null;
+
+        try {
+
+            OkHttpClient okHttpClient = new OkHttpClient();
+            Request.Builder builder = new Request.Builder();
+            Request request = builder.url(params[0]).build();
+            Response response = okHttpClient.newCall(request).execute();
+            return response.body().string();
+
+
+
+
+
+        } catch (Exception e) {
+            Log.d("14MayV1", "e doIn ==> " + e.toString()); //error ในกรณีไม่ต่อเน้ต
+            return null;
+
+        }
+
+
     }
 }//main class
--- app/src/main/java/dru/kotham/narinthon/devnews/MainActivity.java
+++ app/src/main/java/dru/kotham/narinthon/devnews/MainActivity.java
@@ -1,5 +1,6 @@
 package dru.kotham.narinthon.devnews;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,7 +8,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -62,11 +66,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 MyAlert myAlert = new MyAlert(this);
                 myAlert.myDialog("Have Space","Please fill all Erery Black");
 
-
-
             } else {
                 //No Space
                 Log.d("13MayV1","No Space");
+
+                checkUserAnPass();
             }
 
 
@@ -75,6 +79,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //For TextView
         if (v == textView) {
 
+        }
+    }
+
+    private void checkUserAnPass() {
+
+        try {
+            //Create Objece
+            GetAllData getAllData = new GetAllData(this);
+            MyConstant myConstant = new MyConstant();
+            MyAlert myAlert = new MyAlert(this);
+
+            getAllData.execute(myConstant.getUrlGETUser());
+            String strJSON = getAllData.get();
+            Log.d("14MayV1", "JSON ==>" + strJSON);
+
+            //checkUser
+            boolean b = true; //For User false
+            String[] columstring = new String[]{"login_id",
+                    "login_username","login_password","login_status","login_Name"};
+            String[] loginString = new String[columstring.length];
+
+            JSONArray jsonArray = new JSONArray(strJSON);
+            for (int i=0; i<jsonArray.length();i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                if (userString.equals(jsonObject.getString(columstring[1]))) {
+                    b = false;
+                    for (int i1=0;i1<columstring.length;i1++) {
+                        loginString[i1] = jsonObject.getString(columstring[i1]);
+                    }
+                }
+            }//for
+            if (b) {
+                //User False
+                myAlert.myDialog("User false","No This User in my Database");
+            } else if (passwordString.equals(loginString[2])) {
+                //password true
+                Toast.makeText(this, "welcome" + loginString[4], Toast.LENGTH_SHORT).show();
+                //for User
+                if (loginString[3].equals("111")) {
+                    //Intent to Service User
+                    Intent intent = new Intent(MainActivity.this, ServiceUser.class);
+                    intent.putExtra("Login", loginString);
+                    startActivity(intent);
+                    finish();
+                }
+                //for Admin
+                if (loginString[3].equals("500")) {
+                    //Intent to Service Admin
+                }
+
+
+            } else {
+                //Password false
+                myAlert.myDialog("Password false", "Pleasa Try Again Password False");
+            }
+
+
+
+        } catch (Exception e) {
+            Log.d("14MayV1", "e checkUser ==>" + e.toString());
         }
     }
 } // Main Class นี่คือคลาสหลัก
